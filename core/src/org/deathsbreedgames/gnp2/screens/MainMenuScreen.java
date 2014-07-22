1cf9bbebaa4050eeabcf8b238b70c0c9b6f633f7
Deathsbreed <nicolas.ortega.froysa@gmail.com>
2014-07-21 19:50:25 -0500

    Changed the term User for Player.

--- core/src/org/deathsbreedgames/gnp2/screens/MainMenuScreen.java
+++ core/src/org/deathsbreedgames/gnp2/screens/MainMenuScreen.java
@@ -141,10 +141,10 @@ public class MainMenuScreen extends BaseScreen {
 			buttonFont.setColor(0.0f, 1.0f, 0.0f, 1.0f);
 			buttonFont.draw(batch, "Instructions", 195, 450);
 			textFont.setColor(1.0f, 1.0f, 1.0f, 1.0f);
-			textFont.draw(batch, "Left User: W, S", 20, 400);
-			textFont.draw(batch, "Right User: UP, DOWN", 20, 370);
-			textFont.draw(batch, "Top User: V, B", 20, 340);
-			textFont.draw(batch, "Bottom User: COMMA, PERIOD", 20, 310);
+			textFont.draw(batch, "Left Player: W, S", 20, 400);
+			textFont.draw(batch, "Right Player: UP, DOWN", 20, 370);
+			textFont.draw(batch, "Top Player: V, B", 20, 340);
+			textFont.draw(batch, "Bottom Player: COMMA, PERIOD", 20, 310);
 			textFont.draw(batch, "Pause Game: P", 20, 280);
 			textFont.draw(batch, "Quit to Menu: ESC", 20, 250);
 			textFont.draw(batch, "In 1v1v1v1 the objective is to be the last person to touch the ball", 20, 200);
